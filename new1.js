4c5b7dee34a3cabeaf50627b35a97eed74bc18aa
sarthak arora <f20200060@pilani.bits-pilani.ac.in>
2021-04-18 20:19:14 +0530

    Update new1.js

--- new1.js
+++ new1.js
@@ -31,3 +31,24 @@ $(sha).hide();
 $(fin).removeClass("fine")
 
 })}
+
+
+
+
+
+$(document).ready(function(){
+    $('.menu-close').hide();
+
+    $('.menu').click(function(){
+        $('.menu-panel').toggleClass('show');
+        $('.menu-background').toggleClass('show');
+        $('.menu').hide();
+        $('.menu-close').show();
+    });
+    $('.menu-close').click(function(){
+        $('.menu-panel').removeClass('show');
+        $('.menu-background').removeClass('show');
+        $('.menu').show();
+        $('.menu-close').hide();
+    });
+});
