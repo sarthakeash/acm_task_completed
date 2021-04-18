f76e148391a721f094cc040839e28bfbc2cc09a8
sarthak arora <f20200060@pilani.bits-pilani.ac.in>
2021-04-18 11:39:03 +0530

    Update new1.js

--- new1.js
+++ new1.js
@@ -3,24 +3,24 @@ var k;
 for(k=1; k<5; k++){ 
     let com = ".cmnt" +k;
     let cma =".cmntadd" + k;
+    let mng =".mng" + k;
 
 $(com).click(function(){
     let foo = prompt('Please add your comment here');
     if (foo===null) return;
-    if (foo!=""){
+    if (foo!=""){   $(mng).css("display", "inline");
     $(cma).html("COMMENT:<br></br> " + foo);
-    
+    $(cma).css("background-color","rgb(211,211,211,0.2)");
      $(this).hide();}
-
 }
     
 )}
 var i ;
 
-//using for loop for opening tet on click
+//using for loop for opening text on click
 for(i=1; i<5; i++){
     
-let re= ".r" + i ;
+let re= ".p" + i ;
 let hid =".hidden" + i;
 let sha =".shadow" + i;
 let fin = "#fine" + i;
